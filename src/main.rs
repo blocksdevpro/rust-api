ad58595413057f32dd4a214ad8ad6d92acb119eb
BlocksDevPro <blocksdev.pro@gmail.com>
2026-03-05 21:24:51 +0530

    feat: Initialize Axum web server, shared application state, and module structure.

--- src/main.rs
+++ src/main.rs
@@ -9,9 +9,9 @@ async fn main() {
     let state = Arc::new(Mutex::new(state::AppState::new()));
 
     let app = routes::create_router(state);
+    let addr = "0.0.0.0:3000";
 
-    let listener = tokio::net::TcpListener::bind("localhost:3000")
-        .await
-        .unwrap();
+    let listener = tokio::net::TcpListener::bind(addr).await.unwrap();
+    println!("🚀 Server running at http://{}", addr);
     axum::serve(listener, app).await.unwrap();
 }
